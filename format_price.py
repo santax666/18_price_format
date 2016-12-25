ea6c8631e8e3eece0259c1053ba8c66a33830781
Your Name <you@example.com>
2016-12-25 23:12:26 +0300

    Realization of the task

--- format_price.py
+++ format_price.py
@@ -1,5 +1,52 @@
+import argparse
+import re
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(usage='%(prog)s [аргументы]',
+                                     description='Форматирование цены'
+                                                 ' с помощью %(prog)s')
+    parser.add_argument('-p', '--price', help='Цена товара')
+    return parser
+
+
+def has_letter(value):
+    return bool(re.search(r'[А-Яа-яA-Za-zЁё]', value))
+
+
+def has_spec_char(value):
+    return bool(re.search(r'[^А-Яа-яA-Za-zЁё0-9.,]', value))
+
+
+def is_negative_number(price):
+    return bool(price[0] == '-')
+
+
+def get_whole_and_fractional(price):
+    whole_str, fract_str = '{:,.2f}'.format(price).split('.')
+    return whole_str.replace(',', ' '), fract_str
+
+
 def format_price(price):
-    pass
+    if has_letter(price):
+        return 'Цена содержит буквы!', None
+    elif is_negative_number(price):
+        return 'Цена отрицательная!', None
+    elif has_spec_char(price):
+        return 'Цена содержит спецсимволы!', None
+    else:
+        price = price.replace(',', '.')
+        if price.count('.') == 1:
+            return get_whole_and_fractional(float(price))
+        else:
+            return 'Цена некорректная!', None
+
 
 if __name__ == '__main__':
-    pass
+    parser = create_parser()
+    namespace = parser.parse_args()
+    price = namespace.price
+    if price is None:
+        price = input('Введите цену товара для форматирования: ')
+    price_formated = format_price(price)
+    print('.'.join(filter(None, price_formated)))
--- tests.py
+++ tests.py
@@ -1,1 +1,33 @@
-# TODO place your tests here
+import unittest
+import format_price
+
+
+class PriceFormatTestCase(unittest.TestCase):
+    def test_negative_price(self):
+        whole, fractional = format_price.format_price('-123.00545')
+        self.assertEqual(whole, 'Цена отрицательная!')
+        self.assertIsNone(fractional)
+
+    def test_alpha_string_price(self):
+        whole, fractional = format_price.format_price('123.0f564')
+        self.assertEqual(whole, 'Цена содержит буквы!')
+        self.assertIsNone(fractional)
+
+    def test_replace_comma(self):
+        whole, fractional = format_price.format_price('987,304654')
+        self.assertEqual(whole, '987')
+        self.assertEqual(fractional, '30')
+
+    def test_incorrect_price(self):
+        whole, fractional = format_price.format_price('123,123,00654')
+        self.assertEqual(whole, 'Цена некорректная!')
+        self.assertIsNone(fractional)
+
+    def test_correct_price(self):
+        whole, fractional = format_price.format_price('123.4545')
+        self.assertEqual(whole, '123')
+        self.assertEqual(fractional, '45')
+
+
+if __name__ == '__main__':
+    unittest.main()
